01321f464c093fbf5dad9e8a38ad377f2a72376b
Jonathan Reeve <jonathan.reeve@columbia.edu>
2021-11-23 16:47:12 -0500

    move things around

---
+++ content/.dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((citar-bibliography . "references.bib")))
+ (markdown-mode . ((citar-bibliography . "references.bib"))))
